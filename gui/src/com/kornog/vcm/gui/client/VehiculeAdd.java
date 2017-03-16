c4abbd8ff4825c3d80ff0a213b3d5ab3f4dde04e
Gwenole <gwenole@kornog-computing.com>
2017-03-16 17:44:53 +0100

    added vehicule loader

--- gui/src/com/kornog/vcm/gui/client/VehiculeAdd.java
+++ gui/src/com/kornog/vcm/gui/client/VehiculeAdd.java
@@ -1,48 +1,82 @@
 package com.kornog.vcm.gui.client;
 
 import com.google.gwt.core.client.GWT;
-import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.core.client.JsArray;
+import com.google.gwt.core.client.JsonUtils;
+import com.google.gwt.dom.client.TableCellElement;
+import com.google.gwt.dom.client.TableRowElement;
+import com.google.gwt.dom.client.TableSectionElement;
+import com.google.gwt.event.logical.shared.AttachEvent;
+import com.google.gwt.http.client.Request;
+import com.google.gwt.http.client.RequestBuilder;
+import com.google.gwt.http.client.RequestCallback;
+import com.google.gwt.http.client.RequestException;
+import com.google.gwt.http.client.Response;
+import com.google.gwt.http.client.URL;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
-import com.google.gwt.uibinder.client.UiHandler;
 import com.google.gwt.uibinder.client.UiTemplate;
-import com.google.gwt.user.client.Window;
-import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Composite;
-import com.google.gwt.user.client.ui.HasText;
+import com.google.gwt.user.client.ui.HTMLPanel;
 import com.google.gwt.user.client.ui.Widget;
+import com.kornog.vcm.gui.shared.VehiculeDTO;
 
-public class VehiculeAdd extends Composite implements HasText {
+public class VehiculeAdd extends Composite {
+
+	final String API_VEHICULES = "http://127.0.0.1:8080/api/vehicules";
 
 	private static VehiculeAddUiBinder uiBinder = GWT.create(VehiculeAddUiBinder.class);
-	
+
+	@UiField
+	TableSectionElement vehiculesField;
+
+	@UiField
+	HTMLPanel panel;
+
 	@UiTemplate("VehiculeAdd.ui.xml")
 	interface VehiculeAddUiBinder extends UiBinder<Widget, VehiculeAdd> {
 	}
 
 	public VehiculeAdd() {
 		initWidget(uiBinder.createAndBindUi(this));
-	}
+		panel.addAttachHandler(new AttachEvent.Handler() {
 
-	@UiField
-	Button button;
+			@Override
+			public void onAttachOrDetach(AttachEvent event) {
 
-	public VehiculeAdd(String firstName) {
-		initWidget(uiBinder.createAndBindUi(this));
-		button.setText(firstName);
-	}
+				RequestBuilder builder = new RequestBuilder(RequestBuilder.GET, URL.encode(API_VEHICULES));
 
-	@UiHandler("button")
-	void onClick(ClickEvent e) {
-		Window.alert("Hello!");
-	}
+				try {
+					Request request = builder.sendRequest(null, new RequestCallback() {
+						public void onError(Request request, Throwable exception) {
 
-	public void setText(String text) {
-		button.setText(text);
-	}
+						}
 
-	public String getText() {
-		return button.getText();
+						public void onResponseReceived(Request request, Response response) {
+							if (200 == response.getStatusCode()) {
+								
+								JsArray<VehiculeDTO> vehicules = JsonUtils.safeEval(response.getText());
+
+								for (int i = 0, count = vehicules.length(); i < count; i++) {
+									VehiculeDTO v = vehicules.get(i);
+									TableRowElement row = vehiculesField.insertRow(-1);
+									TableCellElement cell = row.insertCell(-1);
+									if (v.getImmat() != null) {
+										cell.setInnerText(v.getImmat());
+									}
+								}
+							} else {
+
+							}
+						}
+					});
+				} catch (RequestException e) {
+					// Couldn't connect to server
+				}
+			}
+
+		});
+
 	}
 
 }
